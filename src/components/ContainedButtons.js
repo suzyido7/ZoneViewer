66a1e619ea11f640b41ed8b1f0612bf768181344
suzyido7 <138656087+suzyido7@users.noreply.github.com>
2023-07-13 19:18:36 +0300

    Update ContainedButtons.js

--- src/components/ContainedButtons.js
+++ src/components/ContainedButtons.js
@@ -3,21 +3,19 @@ import Stack from '@mui/material/Stack';
 
 export default function ContainedButtons({zones, drawZones, selectedShape, setSelectedShape, deleteZone}) {
     const handlePrevShape = () => {
-      console.log(`In handlePrevShape with: ${selectedShape}`)
       selectedShape >= 0 && setSelectedShape(selectedShape - 1)
     }
   
     const handleNextShape = () => {
-      console.log(`In handleNextShape with: ${selectedShape}`)
       selectedShape < zones.length && setSelectedShape(selectedShape + 1)
     }
-  
+    
     return (
       <div>
-        {zones.length > 0 && drawZones()}
+        {drawZones()}
         <Stack direction="row" spacing={2} marginLeft={5}>
-         <Button variant="contained" onClick={handlePrevShape}>Prev Shape</Button>
-         <Button variant="contained" onClick={handleNextShape}> Next Shape</Button>
+         <Button variant="contained" onClick={handlePrevShape} disabled={selectedShape <= 0}>Prev Shape</Button>
+         <Button variant="contained" onClick={handleNextShape} disabled={selectedShape >= zones.length - 1}> Next Shape</Button>
          <Button variant="contained" onClick={deleteZone}>Delete Shape</Button>
        </Stack>
     </div>
